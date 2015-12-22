8e981dc40c779340aa1f004f3f4989b7c5d70cfd
Jonathan Dubin <mail@jondubin.com>
2015-12-22 18:51:11 -0500

    setup basic project scaffolding and build tooling

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+var gulp = require('gulp');
+var sourcemaps = require('gulp-sourcemaps');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var browserify = require('browserify');
+var watchify = require('watchify');
+var babel = require('babelify');
+
+function compile(watch) {
+
+    var props = {
+        debug: true,
+        entries: './src/index.js'
+    };
+
+    var browserifyTransform = browserify(props).transform("babelify", {presets: ["es2015", "react"]});
+    var bundler = watch ? watchify(browserifyTransform): browserifyTransform;
+
+    function rebundle() {
+        bundler.bundle()
+            .on('error', function(err) { console.error(err); this.emit('end'); })
+            .pipe(source('build.js'))
+            .pipe(buffer())
+            .pipe(sourcemaps.init({ loadMaps: true }))
+            .pipe(sourcemaps.write('./'))
+            .pipe(gulp.dest('./build'));
+    }
+
+    if (watch) {
+        bundler.on('update', function() {
+            console.log('-> bundling...');
+            rebundle();
+        });
+    }
+
+    rebundle();
+}
+
+function watch() {
+    return compile(true);
+}
+
+gulp.task('build', function() { return compile(); });
+gulp.task('watch', function() { return watch(); });
+
+gulp.task('default', ['watch']);
